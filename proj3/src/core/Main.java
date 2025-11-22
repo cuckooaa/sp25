3e1a05e6c2e817437e541dcc190830b47ffe8405
tomato <2993150540@qq.com>
2025-11-22 19:44:11 +0800

    completed task: proj3a

--- proj3/src/core/Main.java
+++ proj3/src/core/Main.java
@@ -1,10 +1,12 @@
 package core;
 
+import tileengine.TERenderer;
+import tileengine.TETile;
+
 public class Main {
     public static void main(String[] args) {
-
         // build your own world!
-
+        World world=new World();
 
     }
 }
--- proj3/src/core/World.java
+++ proj3/src/core/World.java
@@ -1,7 +1,171 @@
 package core;
 
+import org.junit.jupiter.api.MethodOrderer;
+import tileengine.TERenderer;
+import tileengine.TETile;
+import tileengine.Tileset;
+
+import edu.princeton.cs.algs4.StdDraw;
+import utils.RandomUtils.*;
+
+import java.util.*;
+
+import static utils.RandomUtils.uniform;
+
+
 public class World {
-
     // build your own world!
+    private static final int WIDTH = 70;
+    private static final int HEIGHT = 40;
+    private static final int SEED=20050512;
+    private static final int numOfRoom=12;
+    private static final int maxSizeOfRoom=8;
 
+    // Create grid of tiles (all null to begin with).
+    TETile[][] world = new TETile[WIDTH][HEIGHT];
+    TERenderer ter = new TERenderer();
+    Random random=new Random(SEED);
+    List<xyInRoom> roomList=new ArrayList<>();
+    HashMap<Integer,Integer> map=new HashMap<>();
+
+    class xyInRoom{
+        int x;
+        int y;
+
+        public xyInRoom(int x,int y){
+            this.x=x;
+            this.y=y;
+        }
+    }
+
+    //constructor
+    public World() {
+        ter.initialize(WIDTH,HEIGHT);
+
+        // Fill grid with NOTHING tiles.
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                world[x][y] = Tileset.NOTHING;
+            }
+        }
+        for(int i=0;i<numOfRoom;i++) {
+            drawRoom();
+        }
+//        formRooms();
+        formHallWays();
+        formWalls();
+
+        ter.renderFrame(world);
+    }
+
+    //initialize and generate the world
+    public void generateWorld(){
+
+    }
+
+    private void formWalls(){
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                if((x==0 || x==WIDTH-1 || y==0 || y==HEIGHT-1)
+                        && world[x][y] != Tileset.NOTHING){
+                    world[x][y] = Tileset.WALL;
+                }
+            }
+        }
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 1; y < HEIGHT; y++) {
+                if(world[x][y-1]!=world[x][y]){
+                    if(world[x][y-1]==Tileset.NOTHING) world[x][y]=Tileset.WALL;
+                    else if(world[x][y]==Tileset.NOTHING) world[x][y-1]=Tileset.WALL;
+                }
+            }
+        }
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 1; x < WIDTH; x++) {
+                if(world[x-1][y]!=world[x][y]){
+                    if(world[x-1][y]==Tileset.NOTHING) world[x][y]=Tileset.WALL;
+                    else if(world[x][y]==Tileset.NOTHING) world[x-1][y]=Tileset.WALL;
+                }
+            }
+        }
+    }
+
+    private void formHallWays(){
+        Iterator<xyInRoom> hallWay=roomList.iterator();
+        xyInRoom start=hallWay.next();
+        xyInRoom end;
+        while (hallWay.hasNext()){
+            end=hallWay.next();
+            drawHallWay(start,end);
+            start=end;
+        }
+    }
+
+    private void drawHallWay(xyInRoom s,xyInRoom e){
+//        int breadth=uniform(random,3,5);
+        int breadth=3;
+        int sx=s.x,sy=s.y;
+        int ex=e.x,ey=e.y;
+        int unitX=sx>ex? -1 : 1;
+        int unitY=sy>ey? -1 : 1;
+        for(int h=sx;unitX*(ex-h)>=0;h+=unitX){
+            for(int i=0;i<breadth;i++) {
+                world[h][sy+i-1] = Tileset.FLOOR;
+            }
+        }
+        for(int v=sy;unitY*(ey-v)>=0;v+=unitY){
+            for(int i=0;i<breadth;i++) {
+                world[ex+i-1][v] = Tileset.FLOOR;
+            }
+        }
+    }
+/**
+ * I tried using this method to make it be more distributed,
+ * but in fact the uniform distribution make the links between rooms interweaved each other,
+ * which leads the final breath of hallway exceed the limit.
+ */
+//    private void formRooms(){
+//        int nums=numOfRoom;
+//        while(nums>0){
+//            int startX=uniform(random,0,WIDTH-maxSizeOfRoom);
+//            int startY=uniform(random,0,HEIGHT-maxSizeOfRoom);
+//            int area=0;
+//            if(startX<8 && startY<5) area=1;
+//            else if((startX>=18 && startX<26) && (startY<5)) area=2;
+//            else if((startX>=36 && startX<44) && (startY<5)) area=3;
+//            else if((startX>=54 && startX<62) && (startY<5)) area=4;
+//            else if((startX<8) && (startY>=13 && startY<18)) area=5;
+//            else if((startX>=18 && startX<26) && (startY>=13 && startY<18)) area=6;
+//            else if((startX>=36 && startX<44) && (startY>=13 && startY<18)) area=7;
+//            else if((startX>=54 && startX<62) && (startY>=13 && startY<18)) area=8;
+//            else if((startX<8) && (startY>=26 && startY<31)) area=9;
+//            else if((startX>=18 && startX<26) && (startY>=26 && startY<31)) area=10;
+//            else if((startX>=36 && startX<44) && (startY>=26 && startY<31)) area=11;
+//            else if((startX>=54 && startX<62) && (startY>=26 && startY<31)) area=12;
+//
+//            if(!map.containsKey(area)){
+//                map.put(area,1);
+//                drawRoom(startX,startY);
+//                nums--;
+//            }
+//            else continue;
+//
+//        }
+//    }
+
+    private void drawRoom(){
+        int startX=uniform(random,0,WIDTH-maxSizeOfRoom);
+        int startY=uniform(random,0,HEIGHT-maxSizeOfRoom);
+        int sizeX=uniform(random,5,maxSizeOfRoom);
+        int sizeY=uniform(random,5,maxSizeOfRoom);
+
+        for(int x=startX;x<startX+sizeX;x++){
+            for(int y=startY;y<startY+sizeY;y++){
+                world[x][y]=Tileset.FLOOR;
+            }
+        }
+        int x=uniform(random,startX+2,startX+sizeX-2);
+        int y=uniform(random,startY+2,startY+sizeY-2);
+        roomList.add(new xyInRoom(x,y));
+    }
 }
